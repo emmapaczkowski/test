5e8ced3a9267a0122712fbcf709f7c774d4fdbe0
{Emma Paczkowski} <{emmapaczkowski@gmail.com}>
2018-11-22 20:08:21 -0500

    first commit

---
+++ byteGetter2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> hexToBytes(int hex, int numBytes) {
+	
+	for (int a = 0; a < numBytes; a++) {
+		int byteGetter = 0XFF << (a * 8);
+		int byte;
+		byte = (byteGetter & hex);
+		byte = byte >> a * 8;
+		ans.push_back(byte);
+	}
+			return (ans);
+
+
+}
+	
+
+int main() {
+	int hex = 0XC9F3, numBytes = 2;
+	vector<int> ans = hexToBytes(hex, numBytes);
+	for (int a = 0; a <= ans.size(); a++)
+		cout << hex << ans[a] << std::endl;
+		
+	return 1;
+}
+
